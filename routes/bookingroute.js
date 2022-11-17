75e91997676b76bd4234996acef5b58df94914c0
Awanish Mishra <55555736+hsinawa@users.noreply.github.com>
2022-11-17 22:07:54 +0530

    Add files via upload

--- routes/bookingroute.js
+++ routes/bookingroute.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const mongoose = require('mongoose')
 
 const Booking = require('../models/Booking')
-
+var nodemailer = require('nodemailer')
 
 accountSid = '23a9b5d52b20b';
 
 authToken = 'b9605668cc8';
 
 const client = require('twilio')('AC4b987f9ad' + accountSid + '55cac0f81f', 'ebffa4353697' + authToken + 'a71c7a16c');
+//zbwxvkpfjigjxant
 
 
 
@@ -55,6 +56,35 @@ Awanish Mishra `,
                         })
                         .then(message => console.log('Message sent', message.sid))
                         .done();
+
+
+const msg = {
+    from :'awanishsampleprojects@gmail.com' ,
+    to:`awanishmishra003@gmail.com` ,
+    subject:`Message from ${req.body.name}` ,
+    text:`Dear Awanish,
+${req.body.name} has sent you a message . The message reads :' ${req.body.message} '. Contact @${req.body.contactnumber} `
+};
+
+nodemailer.createTransport({
+    service:'gmail' ,
+    auth:{
+        user:`awanishsampleprojects@gmail.com` ,
+        pass:'zbwxvkpfjigjxant' 
+
+    } ,
+    port:465,
+    host:`smtp.gmail.com`
+}).sendMail(msg , (err)=>{
+    if(err)
+    {
+        console.log('Error is' , err )
+    }
+    else
+    {
+        console.log('Email sent')
+    }
+} )
 
 
                 }
