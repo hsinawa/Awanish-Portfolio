11b357d6e430a3b86d0a6eef72b44a7b07e16f7d
Awanish Mishra <55555736+hsinawa@users.noreply.github.com>
2023-04-18 23:57:33 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -16,24 +16,20 @@ let dbconnection = require('./auth')
 
 
 
-
 app.use('/api/connect/' , bookingroute )
 app.use('/api/admin' , adminroute )
 
 
-if(process.env.NODE_ENV==='production')
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-{
-    app.use('/' , express.static('client/build') )
-    app.get('*' , (req,res)=>{
-        res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-    } )
-}
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-
+//port
+const port = process.env.PORT || 3333;
 
 
-const port = process.env.PORT ||  7010 ;
 
  app.listen( port , ()=>{
     console.log('Server started of Awanish Site')
